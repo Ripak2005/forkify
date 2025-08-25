44c1578304fc3aafe9775662bed2464262897a57
Ripak Raj <ripakraj2005@gmail.com>
2025-08-25 22:52:50 +0530

    first commit

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+// export const KEY = 'd7ec0c28-d4e9-4e26-b4e9-799d684aefad';
+export const KEY = '9f83d7ff-52e7-4105-ba62-0e0c1f58db9b';
+export const MODAL_CLOSE_SEC = 2.5;
